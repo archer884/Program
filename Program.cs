99919dc581b00608fb70ed07ad0a691706e88961
J/A <archer884@gmail.com>
2015-08-09 02:05:17 -0500

    Refactor (part two)
    Simplify, make more efficient, etc.

--- Program.cs
+++ Program.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FirstAlgorithmPointless
 {
@@ -10,50 +8,25 @@ namespace FirstAlgorithmPointless
     {
         static void Main(string[] args)
         {
-            var randomNumbers = CreateRandomList();
-            var finalNumbers = ProgramLogic(SimpleAlgorithm(randomNumbers));
+            var prng = new Random();
+            var randomNumbers = CreateRandomList(() => prng.Next(98) + 2);
+            var finalNumbers = randomNumbers.Select(Reduce).ToList();
             
             PrintValues(randomNumbers);
             PrintFactors(finalNumbers);
         }
 
-        static IList<int> ProgramLogic(IList<int> values)
+        private static int Reduce(int n)
         {
-            //bool is_entire_list_prime = false;
-            int check = 0;
-            while (check < 100)
+            var max = Math.Sqrt(n) + 1;
+            for (int i = 2; i < max; i++)
             {
-                //display_finalized_list();
-                //System.Threading.Thread.Sleep(10000);
-                check = 0;
-
-                values = Reduce(values);
-                foreach (int i in values)
+                if (n % i == 0)
                 {
-                    if (IsPrime(i))
-                    {
-                        check++;
-                    }
+                    return Reduce(n / i);
                 }
             }
-            return values;
-        }
-
-        private static bool IsPrime(int number)
-        {
-            int i;
-            for (i = 2; i < number; i++)
-            {
-                if (number % i == 0)
-                {
-                    return false;
-                }
-            }
-            if (i == number)
-            {
-                return true;
-            }
-            return false;
+            return n;
         }
 
         static void PrintValues(IList<int> list)
@@ -74,90 +47,13 @@ namespace FirstAlgorithmPointless
             Console.WriteLine("Number of items is " + list.Count);
         }
 
-        static IList<int> Reduce(IList<int> values)
+        static IList<int> CreateRandomList(Func<int> generator)
         {
             var list = new List<int>();
-
-            foreach (int i in values)
-            {
-                if (i % 2 == 0 && i >= 4)
-                {
-                    int x = i / 2;
-                    list.Add(x);
-                }
-                else if (i % 2 != 0 && i >= 4)
-                {
-                    for (int z = 3; z <= i; z++)
-                    {
-                        if (i % z == 0 && i != z)
-                        {
-                            int y = i / z;
-                            list.Add(y);
-                            break;
-                        }
-                        if (i == z)
-                        {
-                            list.Add(z);
-                            break;
-                        }
-                    }
-                }
-                else if (i < 4)
-                {
-                    list.Add(i);
-                }
-            }
-
-            return list;
-        }
-
-        static IList<int> SimpleAlgorithm(IEnumerable<int> source)
-        {
-            var filteredList = new List<int>();
-
-            foreach (int i in source)
-            {
-                if (i % 2 == 0 && i >= 4)
-                {
-                    int x = i / 2;
-                    filteredList.Add(x);
-                }
-                if (i % 2 != 0 && i >= 4)
-                {
-                    for (int z = 3; z <= i; z++)
-                    {
-                        if (i % z == 0 && i != z)
-                        {
-                            int y = i / z;
-                            filteredList.Add(y);
-                            break;
-                        }
-                        if (i == z)
-                        {
-                            filteredList.Add(z);
-                            break;
-                        }
-                    }
-                }
-                if (i < 4)
-                {
-                    filteredList.Add(i);
-                }
-            }
-
-            return filteredList;
-        }
-
-        static IList<int> CreateRandomList()
-        {
-            var rand = new Random();
-            var list = new List<int>();
-
             for (int i = 0; i < 100; i++)
             {
-                list.Add(rand.Next(3, 100));
+                list.Add(generator());
             }
-
             return list;
         }
     }
